75935415a592b3441ca788dc28ed0ce00e479961
pausalv <pausalvador22@gmail.com>
2023-09-05 13:02:31 +0200

    fix: add emport in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,16 @@ export {
   ConnectionStates
 } from "./modules/connection-vmix";
 
+export {
+  TallyArray,
+  TallyStates
+} from "./commands/tally";
+
+export {
+  ActsResponse,
+  ActsActiveResponse,
+  ActsInputResponse,
+  ActsInputVolumeResponse,
+  ActsVolumeResponse
+} from "./commands/acts";
 
